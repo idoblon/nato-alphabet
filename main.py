dc062319d78c928f5556a372823f4647f22044a2
idoblon <enqriquednes@gmail.com>
2025-11-23 19:52:31 +0545

    method added

--- main.py
+++ main.py
@@ -8,7 +8,16 @@ print(data.to_dict())
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-#create a list of hte phonetic code words from a word that the user inputs.
-word = input('Enter a word: ').upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+
+
+def generate_phonetic():
+    #create a list of hte phonetic code words from a word that the user inputs.
+    word = input('Enter a word: ').upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+generate_phonetic()
